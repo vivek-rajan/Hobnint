7b81b1849a200e4521cab91d7f9b09ef36e1f215
Vivek Rajan <vivek@quovantis.com>
2012-04-23 18:02:58 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hobnint::Application.config.secret_token = '01250c6dc99fccad6d1e341792af9ae9aac29df2b0e6a3e1b7fabf8b989cfb14b944840856a11bac1ed506a2c791fe9a98229124063a643db90d8c189d40d96e'
