0b9767f2238ded8ec4323c7e666408903b37eb1f
Vivek Rajan <vivek@quovantis.com>
2012-04-24 15:21:06 +0530

    Assets Precompiled

--- Gemfile
+++ Gemfile
@@ -19,6 +19,9 @@ end
 gem 'jquery-rails'
 
 gem 'pg'
+gem 'heroku'
+gem 'thin'
+gem 'taps'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Hobnint
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile << /(^[^_]|\/[^_])[^\/]*/
+    config.assets.initialize_on_precompile = false
   end
 end
